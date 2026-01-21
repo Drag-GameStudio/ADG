89d65e9b55e7d1e346a83dc37adf9991c6e94b4c
Drag-GamaStudio <sinica911@gmail.com>
2026-01-21 19:20:58 +0200

    add custom modeules

---
+++ autodocgenerator/auto_runner/run_file.py
@@ -0,0 +1,60 @@
+from autodocgenerator.manage import Manager
+from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn
+from autodocgenerator.factory.base_factory import DocFactory
+from autodocgenerator.factory.modules.intro import IntroLinks, IntroText
+from autodocgenerator.ui.progress_base import BaseProgress, LibProgress
+from autodocgenerator.preprocessor.settings import ProjectSettings
+from .config_reader import Config, read_config
+from autodocgenerator.engine.models.gpt_model import GPTModel, AsyncGPTModel
+from autodocgenerator.engine.config.config import API_KEY
+
+def gen_doc(project_settings: ProjectSettings, ignore_list: list[str], project_path: str, doc_factory: DocFactory, intro_factory: DocFactory):
+    
+
+    with Progress(
+        SpinnerColumn(),          
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),               
+        TaskProgressColumn(),     
+    ) as progress:
+
+        sync_model = GPTModel(API_KEY)
+        async_model = AsyncGPTModel(API_KEY)
+        
+        manager = Manager(
+            project_path, 
+            project_settings, 
+            sync_model=sync_model,
+            async_model=async_model,
+            ignore_files=ignore_list, 
+            progress_bar=LibProgress(progress), 
+            language="en")
+
+        manager.generate_code_file()
+        manager.generate_global_info_file(use_async=True, max_symbols=5000)
+        manager.generete_doc_parts(use_async=True, max_symbols=4000)
+        manager.factory_generate_doc(doc_factory)
+        manager.factory_generate_doc(intro_factory)
+
+        return manager.read_file_by_file_key("output_doc")
+
+if __name__ == "__main__":
+    with open("autodocconfig.yml", "r", encoding="utf-8") as file:
+        config_data = file.read()
+    config: Config = read_config(config_data)
+
+    project_settings = config.get_project_settings()
+    doc_factory, intro_factory = config.get_doc_factory()
+
+    output_doc = gen_doc(
+        project_settings,
+        config.ignore_files,
+        ".",
+        doc_factory,
+        intro_factory
+    )
+
+    
+
+
+
