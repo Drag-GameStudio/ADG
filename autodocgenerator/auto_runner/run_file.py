276fa69a21e4af78a3c959840f6a6661db086b36
Drag-GamaStudio <sinica911@gmail.com>
2026-01-23 20:07:25 +0200

    add new type of progress

--- autodocgenerator/auto_runner/run_file.py
+++ autodocgenerator/auto_runner/run_file.py
@@ -2,7 +2,7 @@ from autodocgenerator.manage import Manager
 from rich.progress import Progress, SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn, TimeRemainingColumn
 from autodocgenerator.factory.base_factory import DocFactory
 from autodocgenerator.factory.modules.intro import IntroLinks, IntroText
-from autodocgenerator.ui.progress_base import BaseProgress, LibProgress
+from autodocgenerator.ui.progress_base import BaseProgress, LibProgress, ConsoleGtiHubProgress
 from autodocgenerator.preprocessor.settings import ProjectSettings
 from .config_reader import Config, read_config
 from autodocgenerator.engine.models.gpt_model import GPTModel, AsyncGPTModel
@@ -10,34 +10,25 @@ from autodocgenerator.engine.config.config import API_KEY
 
 def gen_doc(project_settings: ProjectSettings, ignore_list: list[str], project_path: str, doc_factory: DocFactory, intro_factory: DocFactory):
     
+    sync_model = GPTModel(API_KEY)
+    async_model = AsyncGPTModel(API_KEY)
+    
+    manager = Manager(
+        project_path, 
+        project_settings, 
+        sync_model=sync_model,
+        async_model=async_model,
+        ignore_files=ignore_list, 
+        progress_bar=ConsoleGtiHubProgress(), 
+        language="en")
 
-    with Progress(         
-        TextColumn("[progress.description]{task.description}"),
-        BarColumn(),               
-        TaskProgressColumn(),
-        auto_refresh=False,
-        refresh_per_second=0.5     
-    ) as progress:
+    manager.generate_code_file()
+    manager.generate_global_info_file(use_async=False, max_symbols=8000)
+    manager.generete_doc_parts(use_async=False, max_symbols=8000)
+    manager.factory_generate_doc(doc_factory)
+    manager.factory_generate_doc(intro_factory)
 
-        sync_model = GPTModel(API_KEY)
-        async_model = AsyncGPTModel(API_KEY)
-        
-        manager = Manager(
-            project_path, 
-            project_settings, 
-            sync_model=sync_model,
-            async_model=async_model,
-            ignore_files=ignore_list, 
-            progress_bar=LibProgress(progress), 
-            language="en")
-
-        manager.generate_code_file()
-        manager.generate_global_info_file(use_async=False, max_symbols=8000)
-        manager.generete_doc_parts(use_async=False, max_symbols=4000)
-        manager.factory_generate_doc(doc_factory)
-        manager.factory_generate_doc(intro_factory)
-
-        return manager.read_file_by_file_key("output_doc")
+    return manager.read_file_by_file_key("output_doc")
 
 if __name__ == "__main__":
     with open("autodocconfig.yml", "r", encoding="utf-8") as file:
--- autodocgenerator/ui/progress_base.py
+++ autodocgenerator/ui/progress_base.py
@@ -34,3 +34,39 @@ class LibProgress(BaseProgress):
     def remove_subtask(self):
         if self._cur_sub_task is not None:
             self._cur_sub_task = None
+
+
+class ConsoleTask:
+    def __init__(self, name, total_len):
+        self.name = name
+        self.total_len = total_len
+        self.start_task()
+
+    def start_task(self):
+        self.current_len = 0
+        print(f"Starting task: {self.name} with total length {self.total_len}")
+
+    def progress(self):
+        percent = (self.current_len / self.total_len) * 100
+        print(f"Task: {self.name} Progress: {percent:.2f}%")
+
+    
+
+class ConsoleGtiHubProgress(BaseProgress):
+    def __init__(self):
+        super().__init__()
+        self.curr_task = None
+        self.gen_task = ConsoleTask("General Progress", 5)
+
+    def create_new_subtask(self, name: str, total_len: int):
+        self.curr_task = ConsoleTask(name, total_len)
+
+    def update_task(self):
+        if self.curr_task is not None:
+            self.curr_task.progress()
+        else:
+            self.gen_task.progress()
+
+
+    def remove_subtask(self):
+        self.curr_task = None
