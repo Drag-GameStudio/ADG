25cb71989f50bfd43ba51fc5ec7f442e77055195
dima-on <sinica911@gmail.com>
2026-01-28 12:34:51 +0200

    add init

--- autodocgenerator/__init__.py
+++ autodocgenerator/__init__.py
@@ -1,4 +1,24 @@
-print("ADG")
+def _print_welcome():
+    BLUE = "\033[94m"
+    BOLD = "\033[1m"
+    CYAN = "\033[96m"
+    RESET = "\033[0m"
+
+    ascii_logo = f"""
+    {BLUE}{BOLD}    ___     ____     ______
+       /   |   / __ \   / ____/
+      / /| |  / / / /  / / __  
+     / ___ | / /_/ /  / /_/ /  
+    /_/  |_|/_____/   \____/   
+    {RESET}"""
+    
+    print(ascii_logo)
+    print(f"{CYAN}ADG Library{RESET} | {BOLD}Status:{RESET} Ready to work")
+    print(f"{'—' * 35}\n")
+
+_print_welcome()
+
+
 from .ui.logging import BaseLogger, BaseLoggerTemplate, InfoLog, ErrorLog, WarningLog
 
 logger = BaseLogger()
