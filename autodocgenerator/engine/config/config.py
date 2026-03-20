79c511b107e8872a62c8a69c687e5275803557e6
Drag-GamaStudio <67599997+Drag-GameStudio@users.noreply.github.com>
2026-03-20 20:01:27 +0200

    add new model (#12)

--- autodocgenerator/engine/config/config.py
+++ autodocgenerator/engine/config/config.py
@@ -210,9 +210,17 @@ from dotenv import load_dotenv
 
 load_dotenv() 
 GROQ_API_KEYS = os.getenv("GROQ_API_KEYS", "").split(",")
+GH_MODEL_API_KEYS = os.getenv("GH_MODEL_API_KEYS", "").split(",")
+
 GOOGLE_EMBEDDING_API_KEY = os.getenv("GOOGLE_EMBEDDING_API_KEY", "")
 GITHUB_EVENT_NAME = os.getenv("GITHUB_EVENT_NAME", "")
 
 
-if len(GROQ_API_KEYS) == 0:
+if len(GROQ_API_KEYS[0]) < 2:
+    GROQ_API_KEYS = []
+
+if len(GH_MODEL_API_KEYS[0]) < 2:
+    GH_MODEL_API_KEYS = []
+
+if len(GROQ_API_KEYS) == 0 and len(GH_MODEL_API_KEYS) == 0:
     raise Exception("API_KEY is not set in environment variables.")
