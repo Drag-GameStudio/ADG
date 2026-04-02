aa5bfb0fedfa1b3ad6f64842ccd2c0fc731b6236
Drag-GamaStudio <sinica911@gmail.com>
2026-04-03 01:51:44 +0300

    fix

--- autodocgenerator/auto_runner/token_auth.py
+++ autodocgenerator/auto_runner/token_auth.py
@@ -39,8 +39,7 @@ def main():
             print(f"Local result: MODELS_API_KEYS={github_token}, GOOGLE_EMBEDDING_API_KEY={google_token}")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
-        exit(1)
+        pass
 
 if __name__ == "__main__":
     main()
