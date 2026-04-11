e9607c5c4a6c366a4886df87193dfc3affd41b88
Drag-GamaStudio <sinica911@gmail.com>
2026-04-11 17:52:44 +0300

    add output

--- autodocgenerator/auto_runner/post_to_server.py
+++ autodocgenerator/auto_runner/post_to_server.py
@@ -1,5 +1,7 @@
 import os
 import requests
+from autodocgenerator.manage import Manager
+
 
 def main():
     api_key = os.getenv("ADG_API_TOKEN")
@@ -15,7 +17,24 @@ def main():
     )
     result.raise_for_status()
     data = result.json()
+    curr_doc_id = data.data.doc_id
     print(f"Received data: {data}")
+    print(f"Current document ID: {curr_doc_id}")
+
+    with open(".auto_doc_cache/output_doc.md", "r", encoding="utf-8") as f:
+        output_doc_content = f.read()
+    
+
+    result = f"""
+### 🚀 Powered by ADG System
+The original version of this document offers a superior layout and faster navigation. 
+**Check it out here:** [Full Documentation Interface](https://draggame-adg-frontend.hf.space/docs/{curr_doc_id})
+---
+
+{output_doc_content}
+    """
+    with open(".auto_doc_cache/output_doc.md", "w", encoding="utf-8") as f:
+        f.write(result)
 
 if __name__ == "__main__":
     main()
